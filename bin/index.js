eb243899ee947c82d5161a307c4123f7cbf527f9
Brajesh Sachan <brajesh.sachan@gmail.com>
2020-07-09 12:24:15 +0800

    Calling an API

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
 const yargs = require("yargs");
+const axios = require("axios");
 
 const args = yargs
  .usage("Usage: -n <name>")
  .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
  .argv;
- 
-const greeting = `Hello, ${args.name}!`;
 
-console.log(greeting);
+const sal = `Hello, ${args.name}!`;
+console.log(sal);
+
+console.log("Here is a random useless fact for you:");
+
+const url = "https://uselessfacts.jsph.pl/random.json";
+
+axios.get(url).then(res => {
+   console.log(res.data.text);
+ });
